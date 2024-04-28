bc0cf293ee4b3b151e5bf57e4e6dfb7617c9c4bf
zishan344 <zishanahmed344@gmail.com>
2024-04-28 22:35:49 +0600

    solve module 13.1 loop pattern test problem and homework

--- module_13/loop_pattern1.c
+++ module_13/loop_pattern1.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 int main()
 {
-   printf("hello world");
+   
+  int n;
+  int k=1;
+  scanf("%d", &n);
+  // module finish
+/*   for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <=k ; j++)
+    {
+      printf("*");
+    }
+    k++;
+    printf("\n");
+  } */
+  //module homework
+  for (int i = n; i > 0; i--)
+  {
+    k=i;
+      for (int j = k; j >= 1 ; j--)
+    {
+     printf("*");
+    }  
+     
+    // k--;
+    printf("\n");
+  }
+  
+  
   return 0;
 }
