7cbde66c1a3d8db3dc145c0b8663daf093dfc1a0
zishan344 <zishanahmed344@gmail.com>
2024-04-29 16:00:59 +0600

    complete module 12.2 parctic problem

--- module_13/loop_pattern2.c
+++ module_13/loop_pattern2.c
@@ -1,24 +1,42 @@
 #include <stdio.h>
 int main()
 {
-  int n,k=1,s=1;
+  int n,k=1,s=0;
   scanf("%d",&n);
-   for (int i = 1; i <= n; i++)
+      for (int i = 1; i <= n; i++)
    {
-     
     for (int j = n-i; j > 0 ; j--)
     {
      printf(" ");
     }
     for (int j = 1; j <= k; j++)
     {
+    // printf("%d - %d ", j, k);
      printf("*");
     }
     k+=2;
-    
     printf("\n");
-    
-   }
-   
+   }  
+
+printf("\n \n");
+
+    for (int i = n; i > 0; i--)
+    {
+  
+     for (int j = 0; j < s ; j++)
+     {
+       printf(" ");
+     }  
+    k=i+(i-1);
+    for (int j = k; j>0; j--)
+    {
+      printf("*");
+    };
+      
+     s++;
+     printf("\n");
+   }   
+ 
+ 
   return 0;
 }
